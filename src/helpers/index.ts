a9c5374f01d3372d5addb318be3428cda4bd382f
Eric David Smith <erictherobot@gmail.com>
2023-04-14 21:33:17 -0400

    Adding all the things.

---
+++ src/helpers/index.ts
@@ -0,0 +1,8 @@
+export function slugify(text: string): string {
+  return text
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w\-]+/g, "")
+    .replace(/\-\-+/g, "-");
+}
